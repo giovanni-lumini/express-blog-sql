7bf6d25734b3c93c1abe022e49d7495e3004bc68
giovanni-lumini <giovanni.lumini28@gmail.com>
2024-12-06 12:35:13 +0100

    db.js

--- db/db.js
+++ db/db.js
@@ -6,3 +6,10 @@ const connection = mysql.createConnection({
     password: "password",
     database: "express-blog-sql",
 });
+
+connection.connect((err) => {
+    if (err) throw err;
+    console.log("connected to MySQL");
+});
+
+module.exports = connection;
